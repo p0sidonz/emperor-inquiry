2d25153cac2692804be779574497094b1a03e803
Ankit <ethicalankitx0@gmail.com>
2024-03-15 09:37:17 +0530

    rmoved pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
 'use client'
 import { useState } from "react";
-import Image from "next/image";
-
-import PrismaClient from "../utils/prismaclient";
 
 export default function Home() {
   const [formData, setFormData] = useState({
--- src/app/page/_not-found.js
+++
@@ -1,3 +0,0 @@
-export default function CustomNotFound() {
-    return <h1>NoFound - Error</h1>
-  }
